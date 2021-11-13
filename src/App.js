92f9c5c393160ca44d33261431c11468a35c2577
Cayli Henning <cayli.henning@gmail.com>
2021-11-13 16:30:08 -0500

    Home

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
     <Router>
       <div className="app">
         <Routes>
-          <Route path='/' element={<Navigate replace to='/dashboard'/>}/>
+          <Route path='/' element={<Home/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/dashboard'
             element={
