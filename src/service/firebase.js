0e3b3f70e96a570ce59458fad10debac405a0c58
sultalish <a-sultangazin@knights.ucf.edu>
2021-11-12 23:09:49 -0500

    Add SignIn and Firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAnv2RYWJlh-h55aM6YHu0i2K51IMqjjB8",
+    authDomain: "tranquest-cloud.firebaseapp.com",
+    projectId: "tranquest-cloud",
+    storageBucket: "tranquest-cloud.appspot.com",
+    messagingSenderId: "604093046881",
+    appId: "1:604093046881:web:d2224dd3be6678ad28ca20",
+    measurementId: "G-N3GPLQZKJN"
+};
+
+// Initialize Firebase 
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
