9c723632664925b6e46a765015f4025305a77bcd
Cayli Henning <cayli.henning@gmail.com>
2021-11-14 00:47:40 -0500

    XP Bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,31 +5,44 @@ import firebase from '../service/firebase';
 const ProgressBar = () => {
   const [progressWidth, setProgressWidth] = useState(0);
   const [level, setLevel] = useState(0);
+  const [xpLevel, setXPLevel] = useState(0);
 
   useEffect(async() => {
-    let doc = await db.collection('users').doc(auth.currentUser.uid).get();
-    let data = await doc.data();
-    let tasksCompleted = await data.tasksCompleted;
-    let xplevel = await data.xplevel;
-    let level = await data.level;
+    // Get user's level and XP level
+    // let doc = await db.collection('users').doc(auth.currentUser.uid).get();
+    // let data = await doc.data();
+    // let xplevel = await data.xplevel;
+    //let level = await data.level;
+
+    // Get number of tasks completed
+    const tasksRef = await db.collection('users').doc(auth.currentUser.uid).collection('tasks');
+    const snapshot = await tasksRef.where('completed', '==', true).get();
+    const tasks = snapshot.docs.map(doc => doc.data());
+    let tasksCompleted = tasks.length;
+
+    const totalXP = tasksCompleted * 100;
+    const threshold = 100;
+    const calcLevel = 0.5 + Math.sqrt(1 + 8*(totalXP)/(threshold)) / 2;
+
+    setLevel(Math.floor(level));
+    setXPLevel(level - Math.floor(level));
+
+    db.collection('users').doc(auth.currentUser.uid).update({
+      level: level,
+      xplevel: xpLevel,
+    })
 
     console.log(tasksCompleted);
     console.log(level);
-    console.log(xplevel);
-    if (xplevel >= level * 100) {
-      db.collection('users').doc(auth.currentUser.uid).update({
-        level: firebase.firestore.FieldValue.increment(1),
-        xplevel: xplevel - (level * 100),
-      })
-    }
+    console.log(xpLevel);
+    // if (xplevel >= level * 100) {
+    //   db.collection('users').doc(auth.currentUser.uid).update({
+    //     level: firebase.firestore.FieldValue.increment(1),
+    //     xplevel: xplevel - (level * 100),
+    //   })
+    // }
 
-    doc = await db.collection('users').doc(auth.currentUser.uid).get();
-    data = await doc.data();
-    tasksCompleted = await data.tasksCompleted;
-    xplevel = await data.xplevel;
-    level = await data.level;
-
-    const width = xplevel / (level * 100);
+    const width = xpLevel / (level * 100);
 
     setProgressWidth(width);
     setLevel(level);
